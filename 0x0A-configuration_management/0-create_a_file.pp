6c9faf14a052ccd935200768aa103201d0c8d767
Vincent DUSHIME <vincentdx18@gmail.com>
2023-11-27 06:54:41 +0200

    updated 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # creates a file in /tmp
 
 file { '/tmp/school':
-  ensure => file,
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0744',
 }
