bedac0dc6e31c62c4c5f4e3a1db56795139a61cc
Vincidax <vincentdx18@gmail.com>
2024-01-03 23:14:29 -0800

    update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,20 +1,47 @@
 #!/usr/bin/python3
-"""Returns Todo list for a given employee id
-and axports data in the CSV format"""
+"""
+Generate a Todo list for a given employee id and export it to CSV
+"""
+import csv
 import requests
 from sys import argv
 
-if __name__ == "__main__":
+
+def export_to_csv(employee_id):
+    """
+    Generates a CSV file with tasks for the given employee ID
+    """
     url = "https://jsonplaceholder.typicode.com/"
-    id = argv[1]
 
-    user = requests.get(url + f"users/{id}").json()
-    todos = requests.get(url + f"todos", params={"userId": id}).json()
+    user = requests.get(url + f"users/{employee_id}").json()
+    todos = requests.get(url + "todos", params={"userId": employee_id}).json()
 
-    file = f"{id}.csv"
-    with open(file, "w") as f:
-        for item in todos:
-            f.write(
-                f'"{id}","{user["username"]}","{item["completed"]}", '
-                f'"{item["title"]}"\n'
-            )
+    tasks = [
+        (
+            employee_id,
+            user.get('username'),
+            str(task.get("completed")),
+            task.get("title")
+        )
+        for task in todos
+    ]
+
+    filename = f"{employee_id}.csv"
+    with open(filename, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow([
+            "USER_ID",
+            "USERNAME",
+            "TASK_COMPLETED_STATUS",
+            "TASK_TITLE"])
+        csv_writer.writerows(tasks)
+
+    print(f"Data exported to {filename}")
+
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: python3 script_name.py <employee_id>")
+    else:
+        employee_id = argv[1]
+        export_to_csv(employee_id)
