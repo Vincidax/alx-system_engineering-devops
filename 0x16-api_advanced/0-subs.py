fa1710cebb348926d8a6dd6dd90ca07dd79beeba
Vincidax <vincentdx18@gmail.com>
2024-01-17 09:08:35 +0000

    subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+ubs module
+"""
+
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Retrieves the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The subreddit name.
+
+    Returns:
+        int: The number of subscribers,
+	 or 0 if the subreddit is invalid or an error occurs.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Custom User Agent'}  
+
+    try:
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        
+        if 'error' in data or 'subreddit' not in data:
+            return 0  # Invalid subreddit or error in response
+
+        subscribers = data['subreddit']['subscribers']
+        return subscribers
+
+    except requests.RequestException as e:
+        print(f"Error making request: {e}")
+        return 0
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        print("{:d}".format(number_of_subscribers(subreddit)))
